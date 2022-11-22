a9519053be89fe445cd77515f80af7fce15f58ff
jdbutche <jace.dylan55@gmail.com>
2022-11-22 15:16:43 -0700

    Adding account screen

---
+++ navigation/screens/Account.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
+
+
+//HANDLERS
+///////////////////////////////////////////////////////////////////////////
+
+export const handleCreateAccPress = () => {
+    //TODO: Implement logic to navigate to signup page.
+}
+
+export default class Account extends React.Component {
+    state={
+    }
+    
+    render(){
+      return (
+        <View style={styles.container}>
+          <Text>Hydrip Wellness 2023©</Text>
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo:{
+      fontWeight:"bold",
+      fontSize:50,
+      color:"#72d9e9",
+      marginBottom:80
+    }
+  });
