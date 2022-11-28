cf15788b48122c8957cf0942874c6f377ece5b6a
jdbutche <jace.dylan55@gmail.com>
2022-11-28 07:36:03 -0700

    creating new acccount button component.

---
+++ navigation/screens/AccountTabButton.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
+
+
+//HANDLERS
+///////////////////////////////////////////////////////////////////////////
+
+export const handleCreateAccPress = () => {
+    //TODO: Implement logic to navigate to signup page.
+}
+
+export default class AccountTabButton extends React.Component {
+    state={
+    }
+    
+    render(){
+      return (
+        <View style={styles.container}>
+          <Text>Hydrip Wellness 2023©</Text>
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo:{
+      fontWeight:"bold",
+      fontSize:50,
+      color:"#72d9e9",
+      marginBottom:80
+    },
+    style2:{
+        fontWeight:"bold",
+        fontSize:50,
+        color:"#72d9e9",
+        marginBottom:80
+      }
+  });
